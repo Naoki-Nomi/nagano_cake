ca52263964eaaf752f889d9a4e6b2861ff3fe96a
EC2 Default User <ec2-user@ip-172-31-42-241.ec2.internal>
2021-02-05 06:37:42 +0000

    [Update]AdminのItemとGenreの編集画面作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,6 +19,24 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @price = @item.price.to_s(:delimited, delimiter: ',')
+    @tax_price = (@item.price * 1.08).round(0)
+    @tax_price = @tax_price.to_s(:delimited, delimiter: ',')
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_items_path
+  end
+
+
   private
 
   def item_params
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cd3d2c4997d91f8be3606f8b0eefc873ad5458db891e390fe331967d761eefabf5e26e0f90b7adfdf71130365d67a1eb091bcbb66208e6e7787da4635dbef27b'
