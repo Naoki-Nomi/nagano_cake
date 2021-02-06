b894d85fdae95633130ebf393ffe2c91927e10c7
EC2 Default User <ec2-user@ip-172-31-42-241.ec2.internal>
2021-02-06 13:42:40 +0000

    [Add]Customersのshow/edit画面作成

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,31 @@
+class CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(customer_params)
+    redirect_to customers_mypage_path(current_customer.id)
+  end
+
+  def confirm
+  end
+
+  def quit
+    customer = Customer.find(current_customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted, :email)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/about' => 'homes#about'
   resources :items, only: [:index, :show]
-
+  get '/customers/mypage' => 'customers#show'
+  get '/customers/mypage/edit' => 'customers#edit'
+  patch '/customers/mypage' => 'customers#update'
+  get '/customers/mypage/confirm' => 'customers#confirm'
+  patch '/customers/mypage/confirm' => 'customers#quit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
